1b88a9fe8fde3a0364adfec5cb718ed9ea3337ca
jendiamond <thejendiamond@gmail.com>
2016-10-27 17:58:29 -0700

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028001044) do
+ActiveRecord::Schema.define(version: 20161028005449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "baths", force: :cascade do |t|
+    t.integer  "day_id"
+    t.boolean  "bath"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["day_id"], name: "index_baths_on_day_id", using: :btree
+  end
 
   create_table "days", force: :cascade do |t|
     t.integer  "week"
@@ -21,6 +30,15 @@ ActiveRecord::Schema.define(version: 20161028001044) do
     t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "diapers", force: :cascade do |t|
+    t.integer  "day_id"
+    t.string   "diaper_type"
+    t.text     "notes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["day_id"], name: "index_diapers_on_day_id", using: :btree
   end
 
   create_table "feedings", force: :cascade do |t|
@@ -37,5 +55,39 @@ ActiveRecord::Schema.define(version: 20161028001044) do
     t.index ["day_id"], name: "index_feedings_on_day_id", using: :btree
   end
 
+  create_table "pumps", force: :cascade do |t|
+    t.integer  "day_id"
+    t.datetime "pump_time"
+    t.integer  "pump_amount"
+    t.text     "notes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["day_id"], name: "index_pumps_on_day_id", using: :btree
+  end
+
+  create_table "sleeps", force: :cascade do |t|
+    t.integer  "day_id"
+    t.datetime "sleep_start"
+    t.datetime "sleep_end"
+    t.text     "notes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["day_id"], name: "index_sleeps_on_day_id", using: :btree
+  end
+
+  create_table "tummy_times", force: :cascade do |t|
+    t.integer  "day_id"
+    t.integer  "tummy_time"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["day_id"], name: "index_tummy_times_on_day_id", using: :btree
+  end
+
+  add_foreign_key "baths", "days"
+  add_foreign_key "diapers", "days"
   add_foreign_key "feedings", "days"
+  add_foreign_key "pumps", "days"
+  add_foreign_key "sleeps", "days"
+  add_foreign_key "tummy_times", "days"
 end
